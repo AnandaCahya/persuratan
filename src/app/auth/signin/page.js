06b97eb34210c79e0f0545a7e5c068b1da1e3c9d
AnandaCahya <tgigroupjap@gmail.com>
2025-08-05 18:20:58 +0700

    Update theme

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,21 +1,21 @@
 const Signin = () => {
     return (
-        <div className="w-full font-sans items-center justify-items-center h-screen bg-gradient-to-b from-[#000000] to-[#202020]">
+        <div className="w-full font-sans items-center justify-items-center h-screen bg-[#001F3D]">
             <main className="w-full grid grid-cols-2 items-center justify-center h-screen gap-4">
                 <div className="relative w-full flex flex-col items-center justify-center h-full">
-                    <img className="w-full h-screen object-cover object-left-top" src="/assets/img/blackb.jpg"></img>
-                    <span className=" w-2/5 h-full min-h-screen bg-gradient-to-l from-[#000000] to-[rgba(0,0,0,0.1)] absolute top-0 right-0"></span>
+                    <img className="w-full h-screen object-cover object-left-top" src="/assets/img/ocean_background.jpg"></img>
+                    <span className=" w-2/5 h-full min-h-screen bg-gradient-to-l from-[#001F3D] to-[rgba(0,0,0,0)] absolute top-0 right-0"></span>
                 </div>
-                <div className="w-[w-full - w-10] flex flex-col items-center justify-center h-full bg-[#454545] py-10 rounded-l-3xl gap-10 ml-10">
+                <div className="w-[w-full - w-10] flex flex-col items-center justify-center h-full bg-[#067593] py-10 rounded-l-3xl gap-10 ml-10">
                     <p className="w-9/12 text-center text-xl font-semibold border-b p-10 border-white">Masuk</p>
                     <form className="w-4/5 flex flex-col">
                         <label>NIP</label>
-                        <input id="nip" placeholder="Masukkan Nomor induk anda" className="w-full px-12 py-3 bg-[#303030]" inputMode="numeric"></input>
+                        <input id="nip" placeholder="Masukkan Nomor induk anda" className="w-full px-12 py-3 bg-[#001F3D]" inputMode="numeric"></input>
                         <br />
                         <label>Kata sandi</label>
-                        <input id="password" placeholder="Masukkan Kata sandi anda" className="w-full px-12 py-3 bg-[#303030]" type="password"></input>
+                        <input id="password" placeholder="Masukkan Kata sandi anda" className="w-full px-12 py-3 bg-[#001F3D]" type="password"></input>
                     </form>
-                    <button className="rounded-md w-3/5 py-3 bg-[#303030] hover:bg-[#555555] transition-all duration-250">Masuk</button>
+                    <button className="rounded-md w-3/5 py-3 bg-[#055772] hover:bg-[#001F3D] transition-all duration-250">Masuk</button>
                 </div>
             </main>
         </div>
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,19 +2,19 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div className="w-[300px] h-full grid grid-rows-3 p-10 rounded-r-3xl bg-[#303030] fixed">
+        <div className="w-[300px] h-full grid grid-rows-3 p-10 rounded-r-3xl bg-[#067593] fixed">
             <div className="flex flex-col justify-center">
                 <p className="font-semibold text-xs">SiPerDu</p>
                 <p className="text-lg">Ananda Eka Cahyaning Pribadi</p>
                 <p className="text-sm">20 Orang sedang aktif</p>
             </div>
             <div className="flex flex-col justify-center gap-3">
-                <Link className="w-full bg-[#454545] py-3 px-4" href={"/dashboard"}>Dasbor</Link>
-                <Link className="w-full bg-[#454545] py-3 px-4" href={"/pegawai"}>Pegawai</Link>
-                <Link className="w-full bg-[#454545] py-3 px-4" href={"/surat"}>Surat</Link>
+                <Link className="w-full bg-[#055772] py-3 px-4" href={"/dashboard"}>Dasbor</Link>
+                <Link className="w-full bg-[#055772] py-3 px-4" href={"/pegawai"}>Pegawai</Link>
+                <Link className="w-full bg-[#055772] py-3 px-4" href={"/surat"}>Surat</Link>
             </div>
             <div className="flex flex-col justify-center">
-                <button className="hover:bg-[#454545] py-3 px-2 transition-all duration-150">Keluar</button>
+                <button className="hover:bg-[#001F3D] py-3 px-2 transition-all duration-150">Keluar</button>
             </div>
         </div>
     )
